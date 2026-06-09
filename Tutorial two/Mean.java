159505c24c909b81b78a4c41c25b32de3084e027
SHARIF ALI <sshaaarif1@gmail.com>
2026-06-09 04:31:02 +0000

    first commit

---
+++ Tutorial two/Mean.java
@@ -0,0 +1,11 @@
+public class Mean {
+    public static void main(String[] args) {
+        // coding starts here
+        Student s1 = new Student("SHARIF", 23, 3.5);
+        s1.displayInfo();
+        s1.study();
+        s1.displayInfo();
+    }
+}
+
+
