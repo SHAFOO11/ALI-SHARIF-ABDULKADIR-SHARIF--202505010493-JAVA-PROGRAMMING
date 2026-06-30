d72332bf72e57092d7f0efbd263a636206322966
SHARIF ALI <sshaaarif1@gmail.com>
2026-06-30 03:33:01 +0000

    done

---
+++ week_6/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Lecturer dr = new Lecturer("L100", "Dr Sharif", "Java Programming", "Faculty of Information Technology");
+        dr.displayInfo();
+        dr.displaySubject();
+        dr.displayDepartment();
+    }
+}
