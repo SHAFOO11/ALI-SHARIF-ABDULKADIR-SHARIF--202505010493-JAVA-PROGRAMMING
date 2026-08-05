7a3f8b1de57c93a0f51b083d1b491acdb0e11a27
Sharif Abdulkadir Sharif Ali <shariiif77icloud.com@Sharifs-MacBook-Air.local>
2026-08-06 01:45:36 +0800

    week two done

--- week2/Mean.java
+++ week2/Mean.java
@@ -1,13 +1,16 @@
-package week_2;
-
 public class Mean {
+
     public static void main(String[] args) {
-        // coding starts here
         Student s1 = new Student("SHARIF", 23, 3.5);
+
         s1.displayInfo();
         s1.study();
-        s1.displayInfo();
+        s1.takeExam();
+
+        Student s2 = new Student("AHMAD", 21, 3.8);
+
+        System.out.println();
+        s2.displayInfo();
+        s2.study();
     }
 }
-
-
